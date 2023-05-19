67295ef124380c18911e4995c3babf645bd2beba
matanyaniv <matanyaniv@MATANYANIVBFDD>
2023-05-19 17:08:32 +0300

    Add project files.

---
+++ TIcTacToeLogic/TicTacToeGui.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Board = TIcTacToeLogic;
+
+namespace TIcTacToeLogic
+{
+    internal class TicTacToeGui
+    {
+        
+        private const Boolean m_isValid = true;
+        private readonly byte r_MinBoardSize = 3;
+        private readonly byte r_MaxBoardSize = 9;
+        private byte m_BoardSize = 0;
+        private byte m_NumOfPlayers =1;
+        public static void Main(string[] args)
+        {
+            byte[,] board = new byte[3,3]{{ 1,2,1}, { 1,2,2}, { 1,1,2}};
+            printBoard(board);
+
+        }
+
+        /*
+        private byte getBoardSize()
+        {
+            String openingMessage = String.Format(@"Hi, Welcome.
+                Please enter a number in range {0} -{1}, as the board's size.", r_MinBoardSize, r_MaxBoardSize);
+
+            Console.WriteLine(openingMessage);
+            String userInput = Console.ReadLine();
+            int userInputAsInt;
+            bool digitToInt = int.TryParse(userInput, out userInputAsInt);
+            bool isUserInputValid = !m_isValid;
+
+            while(isUserInputValid == !m_isValid && digitToInt==!m_isValid) 
+            {
+                String message = String.Format(@"Wrong input, please enter size board in range {0} - {1}.", r_MinBoardSize, r_MaxBoardSize);
+                Console.WriteLine(message); 
+
+                userInput = Console.ReadLine();
+
+                digitToInt = int.TryParse(userInput,out userInputAsInt);
+                isUserInputValid = isUserNumberValid(userInputAsInt, r_MinBoardSize, r_MaxBoardSize);
+            }
+
+            return (byte)userInputAsInt;
+        }
+
+        private static bool isUserNumberValid(int i_UserInput, int i_MinBoardSize, int i_MaxBoardSize)
+        {
+            bool isInputValid = !m_isValid;
+
+            if (i_UserInput >= i_MinBoardSize || i_MaxBoardSize <= 9) 
+            {
+                isInputValid = m_isValid;
+            }
+
+            return isInputValid;
+        }
+
+        private byte getNumberOfPlayers()
+        {
+            Console.WriteLine("How many players? it's a game for 2.");
+            String userInput = Console.ReadLine();
+            byte numberOfPlayers;
+            bool isInputValidNumber = byte.TryParse(userInput, out numberOfPlayers);
+            
+            while(isInputValidNumber == !m_isValid && m_NumOfPlayers > 2 && m_NumOfPlayers < 1) 
+            { 
+                Console.WriteLine("It's a game for 2, if you would like to play against the computer, press 1, else press 2");
+                userInput = Console.ReadLine();
+                isInputValidNumber = byte.TryParse(userInput, out numberOfPlayers);
+            }
+
+            return numberOfPlayers;
+        }*/
+
+        private static void printBoard(byte[,] i_Board)
+        {
+            int boardSize = i_Board.GetLength(0);
+            String lineSeprator = LineSeprator(boardSize);
+            StringBuilder boardPrint = new StringBuilder();
+
+            for( int i = 0; i < 2 *boardSize; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    if (i == 0)
+                    {
+                        boardPrint.AppendLine(ColumnsNumbers(boardSize));
+                    }
+
+                    else
+                    {
+                        boardPrint.AppendLine(lineSeprator);
+                    }
+                }
+
+                else
+                {
+                    boardPrint.AppendLine(getLineFromBoard(i/2, i_Board));
+                }
+            }
+
+            Console.WriteLine(boardPrint.ToString());
+        }
+
+        private static String LineSeprator(int i_BoardSize)
+        {
+            String slotSeperator = "====";
+            StringBuilder lineSeperator = new StringBuilder();
+
+            for( int i = 0; i < i_BoardSize; i++)
+            {
+                lineSeperator.Append(slotSeperator);
+            }
+
+            lineSeperator.Append("==");
+
+            return lineSeperator.ToString();
+        }
+
+        private static String ColumnsNumbers(int i_BoardSize)
+        {
+            StringBuilder columnsNumbers = new StringBuilder();
+
+            for (int i = 1; i <= i_BoardSize; i++)
+            {
+                if (i == 1)
+                {
+                    columnsNumbers.Append("  " + i.ToString() + "   ");
+                }
+                else
+                {
+                    columnsNumbers.Append(i.ToString() + "   ");
+                }
+
+            }
+
+            return columnsNumbers.ToString();
+        }
+
+        private static String getLineFromBoard(int i_RowIndex, byte[,] i_GameBoard)
+        {
+            StringBuilder lineFromBoard = new StringBuilder();
+            lineFromBoard.Append((i_RowIndex + 1).ToString() + "|");
+            
+            for(int i=0; i < i_GameBoard.GetLength(0); i++)
+            {
+                int currentSlotValue = i_GameBoard[i_RowIndex, i];
+                
+                if(currentSlotValue == 2)
+                {
+                    lineFromBoard.Append("O  |");
+                }
+
+                else if(currentSlotValue == 1)
+                {
+                    lineFromBoard.Append("X  |");
+                }
+
+                else
+                {
+                    lineFromBoard.Append("   |");
+                }
+            }
+            
+            return lineFromBoard.ToString();
+        }
+    }
+}
---
+++ TIcTacToeLogic/TicTacToeLogic.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex02.ConsoleUtils;
+
+
+
+namespace TIcTacToeLogic
+{
+    
+    internal class TicTacToeLogic
+    {
+        internal byte[,] m_Board;
+        private const byte k_AvailableSlotSign = 0;
+
+        private const Boolean m_isValid = true;
+        private int m_NumberOfMovesRemains = 0;
+        private Boolean FirstPlayerMove = true;
+        private readonly byte r_FirstPlayerSign = 1;
+        private readonly byte r_SecondPlayerSign = 2;
+
+        public TicTacToeLogic(int i_BoardSize)
+        {
+            m_NumberOfMovesRemains = i_BoardSize * i_BoardSize;
+            m_Board = new byte[i_BoardSize, i_BoardSize];
+            FirstPlayerMove = true;
+
+        }
+
+        internal bool CanIMakeAMove(int i_Row, int i_Column)
+        {
+            bool response = !m_isValid;
+
+            if (!isThereAWin() && isSlotAvailable(i_Row, i_Column) && m_NumberOfMovesRemains > 0)
+            {
+                response = m_isValid;
+            }
+
+            return response;
+
+        }
+
+        public void PlayMove(int i_Row, int i_Column)
+        {
+            if (CanIMakeAMove(i_Row, i_Column))
+            {
+
+                m_Board[i_Row, i_Column] = getPlayerSign();
+                FirstPlayerMove = (FirstPlayerMove) ? !FirstPlayerMove : FirstPlayerMove;
+                m_NumberOfMovesRemains--;
+            }
+        }
+
+        private Boolean isSlotAvailable(int i_Row, int i_Column) 
+        {
+            bool isSlotAvailable = true;
+
+            if (m_Board[i_Row, i_Column] != k_AvailableSlotSign)
+            {
+
+                isSlotAvailable = false;
+            } 
+
+            return isSlotAvailable;
+        }
+
+        public bool isThereAWin() 
+        {
+            bool isWinning = !m_isValid;
+            bool horizontalWinning = isHorizontalWinning();
+            bool verticalWinning = isVerticalWinning();
+            bool diagonalWinning = isDiagonlWinning();
+
+            if(horizontalWinning || verticalWinning || diagonalWinning)
+            {
+                isWinning = m_isValid;
+            }
+
+            return isWinning;
+        }
+
+        private bool isHorizontalWinning()
+        {
+            bool isThereAnIdenticalRow = !m_isValid;
+
+            for( int row = 0; row < m_Board.GetLength(0); row++)
+            {
+                isThereAnIdenticalRow = rowHorizontallWinning(row);
+
+                if(isThereAnIdenticalRow)
+                {
+                    break;
+                }
+            }
+
+            return isThereAnIdenticalRow;
+        }
+
+        private bool rowHorizontallWinning(int i_RowIndex)
+        {
+            bool isHorizontalIdenticals = m_isValid;
+            byte lastResult = m_Board[i_RowIndex, 0];
+
+            for ( int col = 0; col < m_Board.GetLength(1); col++)
+            {
+                if(lastResult != m_Board[i_RowIndex, col])
+                {
+                    isHorizontalIdenticals = m_isValid; 
+                    break;
+                }
+            }
+
+            return isHorizontalIdenticals;
+        }
+
+        private bool isVerticalWinning()
+        {
+            bool isThereAnIdenticalRow = !m_isValid;
+
+            for (int row = 0; row < m_Board.GetLength(0); row++)
+            {
+                isThereAnIdenticalRow = rowHorizontallWinning(row);
+
+                if (isThereAnIdenticalRow)
+                {
+                    break;
+                }
+            }
+
+            return isThereAnIdenticalRow;
+        }
+
+        private bool colVerticalWinning(int i_ColIndex)
+        {
+            bool isVerticalIdenticals = m_isValid;
+            byte lastResult = m_Board[0, i_ColIndex];
+
+            for (int row = 0; row < m_Board.GetLength(1); row++)
+            {
+                if (lastResult != m_Board[row, i_ColIndex])
+                {
+                    isVerticalIdenticals = m_isValid;
+                    break;
+                }
+            }
+
+            return isVerticalIdenticals;
+        }
+
+        private bool isDiagonlWinning()
+        {
+            bool isElementsAreTheSame = m_isValid;
+            byte lastResult = m_Board[0,0];
+
+            for( int i = 0; i < m_Board.GetLength(0); i++)
+            {
+                byte diagonalElement = m_Board[i,i];
+                
+                if(lastResult != diagonalElement) 
+                { 
+                    isElementsAreTheSame = !m_isValid; 
+                    break;
+                }
+            }
+
+            return isElementsAreTheSame;
+        }
+
+        private byte getPlayerSign()
+        {
+            byte sign = FirstPlayerMove ? r_FirstPlayerSign : r_SecondPlayerSign;
+            return sign;
+        }
+    }
+}
